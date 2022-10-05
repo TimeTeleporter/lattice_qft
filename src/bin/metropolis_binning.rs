027f14f7e07c4822d14ef584e8e99af75c432616
Immauel Albrecht <immanuel@perazim.ch>
2022-10-05 17:50:02 +0200

    Some more binning calculations

---
+++ src/bin/metropolis_binning.rs
@@ -0,0 +1,100 @@
+//! Performs a Metropolis simulation for a given temperature. The resulting
+//! Markov chain gets binned repeatedly to eliminate the correlation between
+//! the calculated observables in order calculate the real error of the
+//! observable.
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+#![feature(generic_arg_infer)]
+#![feature(split_array)]
+
+use std::error::Error;
+
+use serde::{Deserialize, Serialize};
+
+use lattice_qft::{
+    export::{clean_csv, CsvData},
+    lattice::Lattice3d,
+    metropolis::{metropolis_simulation, MarkovChain, MetropolisSimResult},
+};
+
+const TEST_X: usize = 2;
+const TEST_Y: usize = 2;
+const TEST_T: usize = 2;
+
+const BINS_PATH: &str = "data/metropolis_binning/bins1_58.csv";
+const RESULTS_PATH: &str = "data/metropolis_binning/binning_sim_results.csv";
+
+const BURNIN: usize = 100_000; // Number of sweeps until it starts counting.
+const ITERATIONS: usize = 10_000_000;
+
+const TEMP: f64 = 1.58; // Make sure that the path corresponds to the temp
+
+/// Datatype to save and read binned simulation output.
+#[derive(Debug, Serialize, Deserialize)]
+struct MetropolisBinData {
+    temp: f64,
+    binning_step: u32,
+    variance: f64, // The square of the standard deviation
+    std: f64,
+    error: f64,
+}
+
+/// We initialize a 2 by 2 by 2 lattice, on which all possible configurations
+/// with values from 0 to 8 are known. Then we run a metropolis simulation
+/// in order to test that it converges to the desired distribution.
+fn main() {
+    // Initialize the lattice
+    let lattice: Lattice3d<TEST_X, TEST_Y, TEST_T> = Lattice3d::new();
+
+    let (result, data) = metropolis_simulation(&lattice, TEMP, BURNIN, ITERATIONS);
+
+    if let Err(err) = metropolis_output(data, result, TEMP) {
+        eprint!("{}", err);
+    }
+
+    println!("{}: Done", TEMP);
+}
+
+fn metropolis_output(
+    data: MarkovChain,
+    result: MetropolisSimResult,
+    temp: f64,
+) -> Result<(), Box<dyn Error>> {
+    result.clone().read_write_csv(RESULTS_PATH)?;
+
+    clean_csv(BINS_PATH)?;
+
+    for bin_variance in calculate_bin_var(data, temp) {
+        bin_variance.read_write_csv(BINS_PATH)?;
+    }
+
+    Ok(())
+}
+
+fn calculate_bin_var(mut data: MarkovChain, temp: f64) -> Vec<MetropolisBinData> {
+    let mut results: Vec<MetropolisBinData> = Vec::new();
+    let original_length: usize = data.len();
+
+    let mut binning_step: u32 = 0;
+    while 2_usize.pow(binning_step) < original_length {
+        // Calculating the relevant values
+        let variance: f64 = data.calulate_variance();
+        let std: f64 = variance.sqrt();
+        let error: f64 = std / ((data.len() - 1) as f64).sqrt();
+
+        // Printing
+        results.push(MetropolisBinData {
+            temp,
+            binning_step,
+            variance,
+            std,
+            error,
+        });
+
+        // Binning
+        data = data.binning();
+        binning_step += 1;
+    }
+
+    results
+}
--- src/bin/metropolis_test.rs
+++ src/bin/metropolis_test.rs
@@ -11,6 +11,7 @@ use lattice_qft::{
     export::{clean_csv, CsvData},
     field::Field3d,
     lattice::Lattice3d,
+    TEMP_ARY,
 };
 
 const TEST_X: usize = 2;
@@ -36,14 +37,6 @@ struct TestData {
 /// with values from 0 to 8 are known. Then we run a metropolis simulation
 /// in order to test that it converges to the desired distribution.
 fn main() {
-    const TEMP_ARY: [f64; 41] = [
-        0.001, 0.0012589, 0.0015848, 0.0019952, 0.0025118, 0.0031622, 0.0039810, 0.0050118,
-        0.0063095, 0.0079432, 0.01, 0.012589, 0.015848, 0.019952, 0.025118, 0.031622, 0.039810,
-        0.050118, 0.063095, 0.079432, 0.1, 0.12589, 0.15848, 0.19952, 0.25118, 0.31622, 0.39810,
-        0.50118, 0.63095, 0.79432, 1.0, 1.2589, 1.5848, 1.9952, 2.5118, 3.1622, 3.9810, 5.0118,
-        6.3095, 7.9432, 10.0,
-    ];
-
     if let Err(err) = clean_csv(TEST_PATH) {
         eprint!("Cleaning Error: {}", err)
     };
